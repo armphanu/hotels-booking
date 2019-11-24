dcdf1ac677c4b92d4dbd311021813e58362b77f4
BaySP <56307348+BaySP@users.noreply.github.com>
2019-11-25 00:03:42 +0700

    Add files via upload

--- Project.py
+++ Project.py
@@ -14,11 +14,6 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(407, 288)
-        self.room1s = 0
-        self.room2s = 0
-        self.room3s = 0
-        self.room4s = 0
-
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.checkin = QtWidgets.QDateEdit(self.centralwidget)
@@ -29,41 +24,6 @@ class Ui_MainWindow(object):
         self.checkout.setGeometry(QtCore.QRect(120, 100, 110, 22))
         self.checkout.setCalendarPopup(True)
         self.checkout.setObjectName("checkout")
-        self.room1in = QtWidgets.QTextEdit(self.centralwidget)
-        self.room1in.setGeometry(QtCore.QRect(10, 50, 104, 41))
-        self.room1in.setAccessibleName("")
-        self.room1in.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in.setDocumentTitle("")
-        self.room1in.setOverwriteMode(False)
-        self.room1in.setTabStopWidth(200)
-        self.room1in.setPlaceholderText("")
-        self.room1in.setObjectName("room1in")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(10, 100, 104, 41))
-        self.textEdit_2.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.textEdit_2.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.room1in_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.room1in_2.setGeometry(QtCore.QRect(240, 100, 91, 41))
-        self.room1in_2.setAccessibleName("")
-        self.room1in_2.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_2.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_2.setDocumentTitle("")
-        self.room1in_2.setOverwriteMode(False)
-        self.room1in_2.setTabStopWidth(200)
-        self.room1in_2.setPlaceholderText("")
-        self.room1in_2.setObjectName("room1in_2")
-        self.room1in_3 = QtWidgets.QTextEdit(self.centralwidget)
-        self.room1in_3.setGeometry(QtCore.QRect(240, 50, 91, 41))
-        self.room1in_3.setAccessibleName("")
-        self.room1in_3.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_3.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_3.setDocumentTitle("")
-        self.room1in_3.setOverwriteMode(False)
-        self.room1in_3.setTabStopWidth(200)
-        self.room1in_3.setPlaceholderText("")
-        self.room1in_3.setObjectName("room1in_3")
         self.Adultmany = QtWidgets.QSpinBox(self.centralwidget)
         self.Adultmany.setGeometry(QtCore.QRect(340, 50, 61, 21))
         self.Adultmany.setMaximum(2)
@@ -72,55 +32,70 @@ class Ui_MainWindow(object):
         self.Kidmany.setGeometry(QtCore.QRect(340, 100, 61, 21))
         self.Kidmany.setMaximum(2)
         self.Kidmany.setObjectName("Kidmany")
-        self.room1in_4 = QtWidgets.QTextEdit(self.centralwidget)
-        self.room1in_4.setGeometry(QtCore.QRect(10, 0, 101, 41))
-        self.room1in_4.setAccessibleName("")
-        self.room1in_4.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_4.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_4.setDocumentTitle("")
-        self.room1in_4.setOverwriteMode(False)
-        self.room1in_4.setTabStopWidth(200)
-        self.room1in_4.setPlaceholderText("")
-        self.room1in_4.setObjectName("room1in_4")
-        self.type = QtWidgets.QTextEdit(self.centralwidget)
-        self.type.setGeometry(QtCore.QRect(240, 0, 151, 41))
-        self.type.setObjectName("type")
         self.creditcard = QtWidgets.QTextEdit(self.centralwidget)
         self.creditcard.setGeometry(QtCore.QRect(10, 200, 221, 41))
         self.creditcard.setObjectName("creditcard")
-
         self.pushbutton = QtWidgets.QPushButton(self.centralwidget)
         self.pushbutton.setGeometry(QtCore.QRect(240, 200, 61, 41))
         self.pushbutton.setObjectName("pushbutton")
-        self.pushbutton.clicked.connect(self.standard_room)
-
         self.spinBox_3 = QtWidgets.QSpinBox(self.centralwidget)
-        self.spinBox_3.setGeometry(QtCore.QRect(120, 0, 41, 21))
+        self.spinBox_3.setGeometry(QtCore.QRect(120, 10, 41, 21))
         self.spinBox_3.setMaximum(4)
         self.spinBox_3.setObjectName("spinBox_3")
-        self.spinBox_3.valueChanged.connect(self.standard_room)
-
-        self.status = QtWidgets.QTextEdit(self.centralwidget)
-        self.status.setGeometry(QtCore.QRect(170, 0, 61, 41))
-        self.status.setObjectName("status")
-        self.textEdit_5 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_5.setGeometry(QtCore.QRect(10, 150, 104, 41))
-        self.textEdit_5.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.textEdit_5.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.textEdit_5.setObjectName("textEdit_5")
-        self.room1in_5 = QtWidgets.QTextEdit(self.centralwidget)
-        self.room1in_5.setGeometry(QtCore.QRect(240, 150, 91, 41))
-        self.room1in_5.setAccessibleName("")
-        self.room1in_5.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_5.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.room1in_5.setDocumentTitle("")
-        self.room1in_5.setOverwriteMode(False)
-        self.room1in_5.setTabStopWidth(200)
-        self.room1in_5.setPlaceholderText("")
-        self.room1in_5.setObjectName("room1in_5")
         self.pay = QtWidgets.QTextEdit(self.centralwidget)
-        self.pay.setGeometry(QtCore.QRect(340, 150, 61, 41))
+        self.pay.setGeometry(QtCore.QRect(340, 150, 61, 31))
         self.pay.setObjectName("pay")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 0, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 50, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 100, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 150, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(250, 50, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(250, 100, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(250, 150, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(250, 0, 151, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_8.setFont(font)
+        self.label_8.setObjectName("label_8")
+        self.pay_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.pay_2.setGeometry(QtCore.QRect(170, 10, 61, 31))
+        self.pay_2.setObjectName("pay_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 407, 21))
@@ -132,62 +107,21 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-    def standard_room(self):
-        check = self.spinBox_3.value()
-        if check == 1:
-            self.room1
-        print(check)
-    def room1(self):
-        if self.room1s == 0:
-            pass
-    def room2(self):
-        pass
-    def room3(self):
-        pass
-    def room4(self):
-        pass
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.checkin.setDisplayFormat(_translate("MainWindow", "dd/M/yyyy"))
         self.checkout.setDisplayFormat(_translate("MainWindow", "dd/M/yyyy"))
-        self.room1in.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Check-In</span></p>\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p></body></html>"))
-        self.textEdit_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Check-Out</span></p></body></html>"))
-        self.room1in_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt;\">Kid</span></p></body></html>"))
-        self.room1in_3.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">Adult</span></p></body></html>"))
-        self.room1in_4.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:22pt;\">Room</span></p></body></html>"))
         self.pushbutton.setText(_translate("MainWindow", "Booking"))
-        self.textEdit_5.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Credit card</span></p></body></html>"))
-        self.room1in_5.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt;\">Cost</span></p></body></html>"))
+        self.label.setText(_translate("MainWindow", "Room Number"))
+        self.label_2.setText(_translate("MainWindow", "Check-In"))
+        self.label_3.setText(_translate("MainWindow", "Check-Out"))
+        self.label_4.setText(_translate("MainWindow", "Credit Card"))
+        self.label_5.setText(_translate("MainWindow", "Adult"))
+        self.label_6.setText(_translate("MainWindow", "Kid"))
+        self.label_7.setText(_translate("MainWindow", "Cost"))
+        self.label_8.setText(_translate("MainWindow", "Standard Room"))
 
 
 if __name__ == "__main__":
